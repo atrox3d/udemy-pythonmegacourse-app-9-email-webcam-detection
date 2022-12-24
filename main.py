240fe686cfc07783860433d5109ab0d2a58942c0
atrox3d <atrox3d@gmail.com>
2022-12-24 06:51:01 +0100

    add comments for better understanding

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import cv2
 import time
 
-video = cv2.VideoCapture(0)
-time.sleep(1)
+video = cv2.VideoCapture(0)             # get webcam
+time.sleep(1)                           # give webcam time to start
 
 while True:
-    check, frame = video.read()
-    cv2.imshow('videoooo', frame)
+    check, frame = video.read()         # get check and frame
+    print(f'{check=}')
+    cv2.imshow('webcam', frame)         # show webcam frame
 
-    key = cv2.waitKey(1)
-    if key == ord('q'):
+    key = cv2.waitKey(1)                # wait for keyboard press for a millisecond (0 == forever)
+    print(f'{key=}')                    # -1 == no key pressed
+    if key == ord('q'):                 # 'q' == 113
+        print(f'{key=} is "q"')
         break
 
-video.release()
+video.release()                         # close webcam
 
