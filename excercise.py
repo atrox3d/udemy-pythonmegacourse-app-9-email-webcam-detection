f226bd2cd8254dab1185dda1dff4f683217ab49e
atrox3d <atrox3d@gmail.com>
2022-12-26 07:10:07 +0100

    316. Student Project: teacher solution

--- excercise.py
+++ excercise.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import cv2
-import time
+from datetime import datetime
 
 st.title('motion detector')
 
@@ -8,8 +8,8 @@ col1, col2 = st.columns(2)                                      # create two col
 start = col1.button('start camera')
 stop = col2.button('stop camera')
 
-start
-stop
+st.info(f'{start=} {stop=}')
+
 if start:
     camera = cv2.VideoCapture(0)                                # start webcam
 
@@ -19,17 +19,23 @@ if start:
         check, frame = camera.read()                            # get next frame
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)      # convert BGR to RGB
 
-        text = now = time.strftime("%b %d, %Y %H:%M:%S")        # prepare time text
-        cv2.putText(                                            # draw text on current frame
-            img=frame_rgb,                                      # dest frame
-            text=text,
-            org=(0, 50),                                        # bottom left corner of text
-            fontFace=cv2.FONT_ITALIC,
-            fontScale=1,
-            color=(255, 255, 255),
-            thickness=2,
-            lineType=cv2.LINE_AA
-        )
+        # create captions from current time
+        now = datetime.now()
+        captions = [
+            dict(text=now.strftime('%A'), org=(10, 50), color=(255, 255, 255)),
+            dict(text=now.strftime('%H:%M:%S'), org=(10, 100), color=(0, 255, 0))
+        ]
+        for caption in captions:
+            cv2.putText(                                        # draw text on current frame
+                img=frame_rgb,                                  # dest frame
+                text=caption['text'],
+                org=caption['org'],                             # bottom left corner of text
+                fontFace=cv2.FONT_ITALIC,
+                fontScale=1,
+                color=caption['color'],
+                thickness=2,
+                lineType=cv2.LINE_AA
+            )
 
         image.image(frame_rgb)                                  # update image with current frame
 
